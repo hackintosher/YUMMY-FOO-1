3bfb4e0de673d19cd8a0ed00a87bbd3710726a7f
Aiden Woodard <aidenlkw@gmail.com>
2023-11-13 10:50:41 -1000

    new background image

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
 
   const body = document.body;
-  body.style.background = 'url("https://assets.editorial.aetnd.com/uploads/2009/12/gettyimages-1352563243.jpg") center center/cover no-repeat';
+  body.style.background = 'url("https://github.com/aidenlkw/publicimages/raw/e4d6e1945ec72f3e9bb97e27ddd2ebc7e1b64388/Runway%202023-11-13T20_37_49.641Z%20Upscale%20Image%20Upscaled%20Image%203409%20x%201920.jpg") center center/cover no-repeat';
   const handleZoomClick = () => {
     document.body.classList.toggle('zoomed');
   };
