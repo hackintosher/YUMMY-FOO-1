91c42ed76ed3be13d986e7b22fa3430bb7fa1bf4
Kye Steele <kss25@hawaii.edu>
2023-11-22 09:43:53 -1000

    Issue-07: Completed search UI and functionality. Adjusted code in Recipe component to work with search page.

--- app/imports/ui/pages/Search.jsx
+++ app/imports/ui/pages/Search.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Recipes } from '../../api/recipes/Recipes';
 import LoadingSpinner from '../components/LoadingSpinner';
+import Recipe from '../components/Recipe';
 
 const Search = () => {
   const { ready, recipes } = useTracker(() => {
@@ -82,28 +83,10 @@ const Search = () => {
       <Row className="mt-4">
         {data.map((item, index) => (
           <Col key={index} sm={6} md={4} lg={6} className="mb-4">
-            <Card className="h-100 shadow grow-on-hover">
-              <Card.Header style={{ height: '250px', overflow: 'hidden' }}>
-                <Card.Img
-                  variant="top"
-                  src={item.image}
-                  alt={item.name}
-                  style={{ position: 'absolute', top: 0, right: 0, height: '250px', width: '400px', objectFit: 'cover' }}
-                />
-                <Card.Title>{item.name}</Card.Title>
-              </Card.Header>
-              <Card.Body style={{ color: 'black' }}>
-                <Card.Text>Time: {item.time}</Card.Text>
-                <Card.Text>Cost: {item.cost}</Card.Text>
-                <Card.Text>Filter: {item.filter.join(', ')}</Card.Text>
-                <Card.Text>Appliances: {item.appliances.join(', ')}</Card.Text>
-                <Card.Text>Ingredients: {item.ingredients.join(', ')}</Card.Text>
-                <Card.Text>Recipe: {item.recipe}</Card.Text>
-              </Card.Body>
-            </Card>
-
+            <Recipe recipe={item} />
           </Col>
         ))}
+
       </Row>
     </Container>
   ) : (
