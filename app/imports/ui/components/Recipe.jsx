a63c2af1912c621a1dfa7ad03841033e7744dcad
Kye Steele <kss25@hawaii.edu>
2023-11-22 09:48:27 -1000

    Issue-19: Finished Recipe component to appear in card format.

--- app/imports/ui/components/Recipe.jsx
+++ app/imports/ui/components/Recipe.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 /** Renders a single row in the List Recipe (Admin) table. See pages/ListRecipeAdmin.jsx. */
 const Recipe = ({ recipe }) => (
-  <Card className="h-100">
-    <Card.Header>
-      <Image src={recipe.image} width={75} />
-      <Card.Title>{recipe.name}</Card.Title>
-      <Card.Subtitle>{recipe.filter}</Card.Subtitle>
+  <Card className="h-100 shadow grow-on-hover">
+    <Card.Header style={{ height: '250px', overflow: 'hidden' }}>
+      <Card.Img
+        variant="top"
+        src={recipe.image}
+        alt={recipe.name}
+        style={{ position: 'absolute', top: 0, right: 0, maxHeight: '250px', objectFit: 'cover' }}
+      />
     </Card.Header>
-    <Card.Body>
-      <Card.Text>{recipe.time} {recipe.cost}</Card.Text>
-      <footer className="blockquote-footer">{recipe.owner}</footer>
+    <Card.Body style={{ color: 'black' }}>
+      <Card.Title style={{ fontSize: 'xxx-large' }}>{recipe.name}</Card.Title>
+      <Card.Text>Time: {recipe.time}</Card.Text>
+      <Card.Text>Cost: {recipe.cost}</Card.Text>
+      <Card.Text>Filter: {recipe.filter.join(', ')}</Card.Text>
+      <Card.Text>Appliances: {recipe.appliances.join(', ')}</Card.Text>
+      <Card.Text>Ingredients: {recipe.ingredients.join(', ')}</Card.Text>
+      <Card.Text>Recipe: {recipe.recipe}</Card.Text>
     </Card.Body>
   </Card>
 );
