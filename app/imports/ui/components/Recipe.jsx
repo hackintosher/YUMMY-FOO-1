cdd8f01025d30f559f603ba7ade4e14f13e09b5a
Mairi Yoshioka <yoshiokamairi@gmail.com>
2023-11-26 19:08:56 -1000

    link recipe to favorites but favorites is now all recipes

--- app/imports/ui/components/Recipe.jsx
+++ app/imports/ui/components/Recipe.jsx
@@ -4,6 +4,7 @@ import { Card, Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Recipe (Admin) table. See pages/ListRecipeAdmin.jsx. */
 const Recipe = ({ recipe }) => {
@@ -16,28 +17,32 @@ const Recipe = ({ recipe }) => {
   return (
     <Card className="h-100 grow-on-hover" style={{ boxShadow: '0 12px 24px rgba(0, 0, 0, 0.2)', border: 'none', borderRadius: '15px', borderBottomRadius: '0px', overflow: 'hidden', position: 'relative' }}>
       <Card.Header style={{ height: '250px', overflow: 'hidden', position: 'relative' }}>
-        <Card.Img
-          variant="top"
-          src={recipe.image}
-          alt={recipe.name}
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 0,
-            maxHeight: '250px',
-            objectFit: 'cover',
-            borderTop: '15px',
-          }}
-        />
+        <Link to={`/examplerecipe/${recipe._id}`}>
+          <Card.Img
+            variant="top"
+            src={recipe.image}
+            alt={recipe.name}
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              left: 0,
+              bottom: 0,
+              maxHeight: '250px',
+              objectFit: 'cover',
+              borderTop: '15px',
+            }}
+          />
+        </Link>
       </Card.Header>
       <Card.Body style={{ color: 'black', position: 'relative', zIndex: 1 }}>
         <Row>
           <Col>
-            <Card.Title style={{ fontSize: 'xx-large', marginBottom: '10px' }}>
-              {recipe.name}
-            </Card.Title>
+            <Link to={`/examplerecipe/${recipe._id}`}>
+              <Card.Title style={{ fontSize: 'xx-large', marginBottom: '10px' }}>
+                {recipe.name}
+              </Card.Title>
+            </Link>
           </Col>
           <Col>
             {/* eslint-disable-next-line react/button-has-type,jsx-a11y/control-has-associated-label */}
@@ -70,16 +75,16 @@ const Recipe = ({ recipe }) => {
 // Require a document to be passed to this component.
 Recipe.propTypes = {
   recipe: PropTypes.shape({
-    name: String,
-    image: String,
-    time: String,
-    cost: String,
-    filter: String, // Make array?
-    appliances: String, // Make array?
-    ingredients: String, // Make array
-    recipe: String,
-    owner: String,
-    _id: String,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    time: PropTypes.string,
+    cost: PropTypes.string,
+    filter: PropTypes.string, // Make array?
+    appliances: PropTypes.string, // Make array?
+    ingredients: PropTypes.string, // Make array
+    recipe: PropTypes.string,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
--- app/imports/ui/pages/ExampleRecipe.jsx
+++ app/imports/ui/pages/ExampleRecipe.jsx
@@ -1,36 +1,78 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+/* import { Col, Row, Container, Card } from 'react-bootstrap'; */
 import { useTracker } from 'meteor/react-meteor-data';
+import { useParams } from 'react-router';
 import { Recipes } from '../../api/recipes/Recipes';
 import LoadingSpinner from '../components/LoadingSpinner';
+import Recipe from '../components/Recipe';
 
 const ExampleRecipe = () => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id } = useParams();
+  // console.log('EditContact', _id);
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, recipeInfo } = useTracker(() => {
-    // Note that this subscription will get cleaned up
-    // when your component is unmounted or deps change.
+  const { recipe, ready } = useTracker(() => {
     // Get access to Stuff documents.
     const subscription = Meteor.subscribe(Recipes.userPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
-    // Get the ContactAdmin documents
-    const recipeItems = Recipes.collection.find({}).fetch();
+    // Get the document
+    const recipeItem = Recipes.collection.findOne(_id);
     return {
-      recipeInfo: recipeItems,
+      recipe: recipeItem,
       ready: rdy,
     };
-  }, []);
+  }, [_id]);
 
   return (ready ? (
-    <Row className="justify-content-center">
-      <Col>
-        <Row>
-          {recipeInfo.map((recipe) => (<Col key={recipe._id}><Recipes recipe={recipe} /></Col>))}
-        </Row>
-      </Col>
-    </Row>
+  /*
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={10}>
+          <Card>
+            <Card.Img variant="top" src={recipe.image} height="400px" />
+            <Card.Body>
+              <Row>
+                <Col>
+                  <Row><Card.Title>{recipe.name}</Card.Title></Row>
+                  <Row>
+                    <Card.Text>Appliances: {recipe.appliances.join(', ')}</Card.Text>
+                    <Card.Text>Ingredients: {recipe.ingredients.join(', ')}</Card.Text>
+                    <Col><Card.Text>Cost Per Serving: {recipe.cost}</Card.Text></Col>
+                    <Col><Card.Text>Time: {recipe.time}</Card.Text></Col>
+                    <Col><Card.Text>Filter: {recipe.filter.join(', ')}</Card.Text></Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col><Card.Text>Recipe</Card.Text></Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+    */
+    <Recipe recipe={recipe} />
   ) : <LoadingSpinner />);
+};
+
+// Require a document to be passed to this component.
+ExampleRecipe.propTypes = {
+  recipe: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    time: PropTypes.string,
+    cost: PropTypes.string,
+    filter: PropTypes.string, // Make array?
+    appliances: PropTypes.string, // Make array?
+    ingredients: PropTypes.string, // Make array
+    recipe: PropTypes.string,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
 };
 
 export default ExampleRecipe;
