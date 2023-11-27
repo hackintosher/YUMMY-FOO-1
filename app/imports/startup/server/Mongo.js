85ef0d114be9e56a23331708f6223efe918705ba
DavidRickards <davidar@hawaii.edu>
2023-11-27 09:50:09 -1000

    creating FavRecipes Collection pt1

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { FavRecipes } from '../../api/stuff/FavRecipes';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,18 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+const addFavRecipes = (favorite) => {
+  console.log(`  Adding: ${favorite.name} (${favorite.owner})`);
+  FavRecipes.collection.insert(favorite);
+};
+
+// Initialize the StuffsCollection if empty.
+if (FavRecipes.collection.find().count() === 0) {
+  if (Meteor.settings.favoriteData) {
+    console.log('Creating default favorites data (is empty).');
+    Meteor.settings.favoriteData.forEach(favorite => addFavRecipes(favorite));
   }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { FavRecipes } from '../../api/stuff/FavRecipes';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
@@ -8,6 +9,16 @@ Meteor.publish(Stuffs.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Stuffs.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+// User-level publication.
+// If logged in, then publish documents owned by this user. Otherwise, publish nothing.
+Meteor.publish(FavRecipes.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return FavRecipes.collection.find({ owner: username });
   }
   return this.ready();
 });
