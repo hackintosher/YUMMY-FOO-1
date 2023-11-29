0f51c241675519f63f6f4ed546d5c387e523745a
Kye Steele <kss25@hawaii.edu>
2023-11-29 10:06:07 -1000

    Revert "Merge branch 'issue-15' into main"
    This reverts commit bb4e3cfc240718ceeb206fa22d7e5c19c0564985, reversing
    changes made to 10fe02321a066ad6650b5077f47929baf1da3856.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -21,6 +21,7 @@ import About from '../pages/About';
 import ExampleRecipe from '../pages/ExampleRecipe';
 import AddRecipe from '../pages/AddRecipe';
 import Favorites from '../pages/Favorites';
+import Search from '../pages/Search';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => {
